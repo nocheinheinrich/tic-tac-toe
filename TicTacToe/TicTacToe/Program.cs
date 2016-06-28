a899a07dc1cfb83cabdf7565c4fd7b3eb18bc65b
boostar <boostar@outlook.de>
2016-06-28 14:01:10 +0200

    2. Commit ca. 80% sind fertig

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -12,82 +12,100 @@ namespace TTT
 {
   class Program
   {
+    static string cmd;                                              //Anwendereingabe in Kleinbuchstaben
+    static string[] a = new string[3] { "X", "0", " " };
+    static string[] b = new string[3] { "0", "0", "X" };
+    static string[] c = new string[3] { "X", "X", "0" };
+    //static string[] a = new string[3] { " ", " ", " " };
+    //static string[] b = new string[3] { " ", " ", " " };
+    //static string[] c = new string[3] { " ", " ", " " };
+    static string spielstein;
+
     static void Main(string[] args)
     {
-      string spielstein;
-      string status = "weiter";                         //Mögliche Werte: ende = Spiel ist beendet, weiter = Spiel geht weiter
-      bool spieler1 = false;                            //Entscheidet ob X oder 0
-      string komando = "neu";                           //Voreinstellung für ein leeres Spielfeld
-      Spielbrett Spielfeld = new Spielbrett();
-      string cmd;                                      //Anwendereingabe in Kleinbuchstaben
-      string[] a = new string[3] { " ", " ", " " };
-      string[] b = new string[3] { " ", " ", " " };
-      string[] c = new string[3] { " ", " ", " " };
+      string status1 = "weiter";                                     //Mögliche Werte: ende = Spiel ist beendet, weiter = Spiel geht weiter
+      string status2 = "weiter";                                     //Mögliche Werte: ende = Spiel ist beendet, weiter = Spiel geht weiter
+      bool spieler1 = false;                                        //Entscheidet ob X oder 0
+      string komando = "neu";                                       //Voreinstellung für ein leeres Spielfeld
+      int zahl = 2;                                                 //1 oder 2 (für die Ausgabe des Gewinners)
 
-      Spielfeld.ZeichneSpielfeld(komando, a, b, c);    //Erzeugt ein leeres Spielfeld
-
-      // ******   Schleife   ******
-      while (status == "weiter")
+      // ****************************   Äußere Schleife Start   ***************************************
+      while ((komando.ToLower() != "ende") && ((status1 == "weiter") && (status2 == "weiter")))
       {
-        //ToDo: Evtl. problematisch, falls der Anwender eine ungültige Eingabe macht => Spielfeld wird neu gezeichnet
-        spieler1 = !spieler1;                           //Bei der 1. Eingabe bekommt "spieler1" den Wert "true", bei der 2. Eigabe den Wert "False"
+        Spielbrett Spielfeld = new Spielbrett();
+        Spielfeld.ZeichneSpielfeld(komando, a, b, c);                 //Erzeugt ein leeres Spielfeld
 
-        Console.WriteLine("Komando: ");
-        komando = Console.ReadLine();                   //Anwendereingabe (neu, ende, a0, ..., c2)
-        cmd = komando.ToLower();
-
-        if (spieler1)
-          spielstein = "X";
-        else
-          spielstein = "0";
-
-        switch (cmd)
+        // ****************************   Innere Schleife Start   ***********************************
+        while (status1 == "weiter" && status2 == "weiter")
         {
-          case "neu":
-            //Anwendung neu starten ???????
-            break;
-          case "ende":
-            //Anwendung beenden
-            Spielfeld.SpielEnde();
-            break;
-          case "a0":
-            a[0] = spielstein;
-            break;
-          case "a1":
-            a[1] = spielstein;
-            break;
-          case "a2":
-            a[2] = spielstein;
-            break;
-          case "b0":
-            b[0] = spielstein;
-            break;
-          case "b1":
-            b[1] = spielstein;
-            break;
-          case "b2":
-            b[2] = spielstein;
-            break;
-          case "c0":
-            c[0] = spielstein;
-            break;
-          case "c1":
-            c[1] = spielstein;
-            break;
-          case "c2":
-            c[2] = spielstein;
-            break;
-          default:
-            break;
+          //ToDo: Evtl. problematisch, falls der Anwender eine ungültige Eingabe macht => Wahrheitswert darf nicht geändert werden
+          spieler1 = !spieler1;                                                 //Bei der 1. Eingabe bekommt "spieler1" den Wert "true", 
+                                                                                //    bei der 2. Eigabe bekommt "spieler1" den Wert "False"
+          komando = Console.ReadLine();//.ToLower();                                         //Anwendereingabe (neu, ende, a0, ..., c2)
+          //Console.WriteLine(komando);
+          cmd = komando.ToLower();                                              //Alles in Kleinbuchstaben
+          spielstein = SpielsteinBestimmen(spieler1);                           //Welcher Spieler setzt den Stein
+          AntwortAuswerten();                                                   //Auswerten der Eingabe
+          Spielfeld.ZeichneSpielfeld(komando, a, b, c);                         //Spielfeld neu zeichnen
+          status1 = Spielfeld.IstSpielGewonnen(spieler1, a, b, c, zahl);         //Prüfe: Ist das Spiel gewonnen, falls ja => status1 = "ende"
+          status2 = Spielfeld.IstSpielUnentschieden(status1, a, b, c);                    //Prüfe: Ist das Spiel unentschieden, falls ja => status2 = "ende"
         }
-
-        Spielfeld.ZeichneSpielfeld(komando, a, b, c);   //Zeichne Spielfeld neu
+        // ****************************   Innere Schleife Ende   ***********************************
       }
-
+      // ****************************   Äußere Schleife Ende   *****************************************
+    }
+    static string SpielsteinBestimmen(bool spieler1)
+    {
+      //Hier wird festgelegt welcher Stein als nächster gesetzt wird     
+      if (spieler1)
+        spielstein = "X";
+      else
+        spielstein = "0";
+      return spielstein;
     }
 
+    static void AntwortAuswerten()
+    {
+      switch (cmd)
+      {
+        //case "neu":
+        //  Spiel neu starten ???????
+        //  break;
+        //case "ende":
+        //  Spiel beenden ???????
+        //  break;
+        case "a0":
+          a[0] = spielstein;
+          break;
+        case "a1":
+          a[1] = spielstein;
+          break;
+        case "a2":
+          a[2] = spielstein;
+          break;
+        case "b0":
+          b[0] = spielstein;
+          break;
+        case "b1":
+          b[1] = spielstein;
+          break;
+        case "b2":
+          b[2] = spielstein;
+          break;
+        case "c0":
+          c[0] = spielstein;
+          break;
+        case "c1":
+          c[1] = spielstein;
+          break;
+        case "c2":
+          c[2] = spielstein;
+          break;
+        default:
+          break;
+      }
+    }
   }
-
 }
 
 
--- TicTacToe/TicTacToe/Spielbrett.cs
+++ TicTacToe/TicTacToe/Spielbrett.cs
@@ -18,6 +18,7 @@ namespace TTT
       Console.WriteLine("----+---+---");
       Console.WriteLine("2 {0} | {1} | {2} ", a[2], b[2], c[2]);
       Console.WriteLine("            ");
+      Console.WriteLine("Komando: ");
     }
 
     public void SpielEnde()
@@ -26,7 +27,85 @@ namespace TTT
       Environment.Exit(0);
     }
 
+    public string IstSpielGewonnen(bool spieler1, string[] a, string[] b, string[] c, int zahl)
+    {
+      if (spieler1)
+        zahl = 1;
+
+      if (a[0] == a[1] && a[1] == a[2] && a[0] != " ")                //Drei gleiche Steine in einer Spalte
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (b[0] == b[1] && b[1] == b[2] && b[0] != " ")           //Drei gleiche Steine in einer Spalte
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (c[0] == c[1] && c[1] == c[2] && c[0] != " ")           //Drei gleiche Steine in einer Spalte
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (a[0] == b[0] && b[0] == c[0] && b[0] != " ")           //Drei gleiche Steine in einer Zeile
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (a[1] == b[1] && b[1] == c[1] && b[1] != " ")           //Drei gleiche Steine in einer Zeile
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (a[2] == b[2] && b[2] == c[2] && b[2] != " ")           //Drei gleiche Steine in einer Zeile
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (a[0] == b[1] && b[1] == c[2] && b[1] != " ")           //Drei gleiche Steine in einer Diagonale
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else if (a[2] == b[1] && b[1] == c[0] && b[1] != " ")           //Drei gleiche Steine in einer Diagonale
+      {
+        Console.WriteLine("Spieler {0} hat gewonnen! ", zahl);
+        Console.ReadLine();
+        return "ende";
+      }
+      else
+        return "weiter";
+    }
+
+    public string IstSpielUnentschieden(string status1, string[] a, string[] b, string[] c)
+    {
+      //Solange mindestens ein Feld leer ist, ist geht das Spiel noch weiter sofern keiner gewonnen hat (status1 = "ende")
+      //ToDo: Kann man abfragen ob sich der Wert einer Variablen geändert hat?
+      if (status1 == "ende")
+      {
+        return "weiter";
+      }
+      else if ((a[0] == " " || a[1] == " " || a[2] == " " || b[0] == " " || b[1] == " " || b[2] == " " || c[0] == " " || c[1] == " " || c[2] == " "))
+      {
+        return "weiter";
+      }
+      else
+      {
+        Console.WriteLine("Das Spiel endete unentschieden!");
+        Console.ReadLine();
+        return "ende";
+      }
+
+    }
 
   }
 }
+
 
